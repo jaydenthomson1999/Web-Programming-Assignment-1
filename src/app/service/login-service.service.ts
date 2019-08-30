a8cbbcfafa05fae691dc28c0f1a47a5c32eff12a
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-30 14:10:31 +1000

    implmented login service

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Post {
+  ok: boolean;
+  username: string;
+  email: string;
+  type: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
+  private url = 'http:/localhost:3000/login';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public login(username: string, password: string) {
+    this.http.post<Post>(this.url, {username, password}).subscribe(
+      res => {
+        if (res.ok) {
+          sessionStorage.setItem('user',
+          JSON.stringify({
+            username: res.username,
+            email: res.email,
+            type: res.type
+          }));
+        }
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.error);
+      }
+    );
+  }
 }

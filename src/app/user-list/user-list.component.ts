cff847b967fda6c7ba7aff9abd4eb9442e20b0bd
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-30 20:51:49 +1000

    get user list

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Get {
+  ok: boolean;
+  users: any;
+}
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +13,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  private url = 'http://localhost:3000/api/get-users';
+  private users;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.get_users();
+   }
 
   ngOnInit() {
+  }
+
+  get_users() {
+    const data = new Promise((resolve, reject) => {
+      this.http.get<Get>(this.url).subscribe(
+        res => {
+          if (res.ok) {
+            resolve(res.users.users);
+          } else {
+            resolve(false);
+          }
+        },
+        (err: HttpErrorResponse) => {
+          console.log(err.error);
+          reject(err.error);
+        }
+      );
+    });
+
+    data.then(json => {
+      this.users = json;
+    });
   }
 
 }
